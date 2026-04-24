173a07ecfcf4c026498e27d89d53218afa4651a1
Guest4901244 <1455cagdass@gmail.com>
2026-04-25 00:32:01 +0300

    Fixed site

--- site.js
+++ site.js
@@ -4,13 +4,11 @@ function updateBackground(url) {
         bgElement.style.backgroundImage = `url('${url}')`;
         localStorage.setItem('customBG', url);
     } else {
-        // Reset to default gradient if empty
         bgElement.style.backgroundImage = '';
         localStorage.removeItem('customBG');
     }
 }
 
-// Add this line inside your window.onload function to load saved backgrounds
 const savedBG = localStorage.getItem('customBG');
 if (savedBG) {
     document.getElementById('bg-input').value = savedBG;
